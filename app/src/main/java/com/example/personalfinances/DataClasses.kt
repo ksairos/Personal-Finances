05f658344e7756b4cbbc00c92c405d192addbee8
ksairos <www.kim.2000@gmail.com>
2022-09-23 17:02:08 +0900

    Finally finished those Circle Icons and now starting with RecyclerView and Room

--- app/src/main/java/com/example/personalfinances/DataClasses.kt
+++ app/src/main/java/com/example/personalfinances/DataClasses.kt
@@ -1,15 +1,15 @@
-//package com.example.personalfinances
-//
-//data class Category(
-//    val id: String,
-//    var name: String,
-//    var expanses: Int,
-//    var icon: String,
-//    var color: String,
-//)
-//
-//data class Transaction(
-//    val id: String,
-//
-//)
+package com.example.personalfinances
+
+data class Category(
+    val id: String,
+    var name: String,
+    var expanses: Int,
+    var icon: Int,
+    var color: String,
+)
+
+data class Transaction(
+    val id: String,
+
+)
 
--- app/src/main/java/com/example/personalfinances/MainActivity.kt
+++ app/src/main/java/com/example/personalfinances/MainActivity.kt
@@ -14,5 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+
     }
 }
