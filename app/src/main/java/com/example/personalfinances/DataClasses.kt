10586dca8c84ff70505f151c7b368d3dc3e19aea
ksairos <www.kim.2000@gmail.com>
2022-09-23 11:17:34 +0900

    Added bot nav menu, some icons and gradle implementations

---
+++ app/src/main/java/com/example/personalfinances/DataClasses.kt
@@ -0,0 +1,15 @@
+//package com.example.personalfinances
+//
+//data class Category(
+//    val id: String,
+//    var name: String,
+//    var expanses: Int,
+//    var icon: String,
+//    var color: String,
+//)
+//
+//data class Transaction(
+//    val id: String,
+//
+//)
+
--- app/src/main/java/com/example/personalfinances/MainActivity.kt
+++ app/src/main/java/com/example/personalfinances/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.personalfinances
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.personalfinances.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
