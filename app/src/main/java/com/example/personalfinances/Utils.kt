9fef81ef88dfb7805fc8dd9dd408b4b12c67a3a2
ksairos <www.kim.2000@gmail.com>
2022-09-26 14:36:43 +0900

    New colors, new icons and new utils

---
+++ app/src/main/java/com/example/personalfinances/Utils.kt
@@ -0,0 +1,12 @@
+package com.example.personalfinances
+
+import android.content.res.TypedArray
+import androidx.appcompat.app.AppCompatActivity
+
+class Utils {
+    companion object{
+        const val CAT_NAME_KEY: String = "catNameKey"
+        const val CAT_ICON_KEY: String = "catIconKey"
+        const val CAT_COLOR_KEY: String = "catColorKey"
+    }
+}
