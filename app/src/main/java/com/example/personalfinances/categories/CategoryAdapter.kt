7d43416a53642752c1ffb354f16b7dff8eb8aca1
ksairos <www.kim.2000@gmail.com>
2022-10-12 12:01:45 +0900

    Just commented some code

--- app/src/main/java/com/example/personalfinances/categories/CategoryAdapter.kt
+++ app/src/main/java/com/example/personalfinances/categories/CategoryAdapter.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.personalfinances.MakeTransactionActivity
+//import com.example.personalfinances.MakeTransactionActivity
 import com.example.personalfinances.R
 import com.example.personalfinances.Utils
 import com.example.personalfinances.data.Category
@@ -28,15 +28,15 @@ class CategoryAdapter(private val mContext: Context?): ListAdapter<Category, Cat
             category.icon?.let { categoryIcon.setIconResource(it) }
             val text = "$" + category.expanses?.toString()
             categoryPrice.text = text
-            categoryIcon.setOnClickListener{ startTransaction(mContext) }
+//            categoryIcon.setOnClickListener{ startTransaction(mContext) }
 
         }
 
-        private fun startTransaction(mContext: Context?) {
-            val intent = Intent(mContext, MakeTransactionActivity::class.java)
-            intent.putExtra(Utils.TRANSACTION_TO_KEY, binding.categoryName.text.toString())
-            mContext?.startActivity(intent)
-        }
+//        private fun startTransaction(mContext: Context?) {
+//            val intent = Intent(mContext, MakeTransactionActivity::class.java)
+//            intent.putExtra(Utils.TRANSACTION_TO_KEY, binding.categoryName.text.toString())
+//            mContext?.startActivity(intent)
+//        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
